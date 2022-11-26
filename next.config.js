befc2547d34d776b54d615eded17f9244e568b90
dicechick373 <dicechick373@gmail.com>
2022-11-26 20:27:28 +0900

    logo.svgを作成

--- next.config.js
+++ next.config.js
@@ -3,27 +3,39 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 
 // Remove this if you're not using Fullcalendar features
-const withTM = require('next-transpile-modules')([
-  '@fullcalendar/common',
-  '@fullcalendar/react',
-  '@fullcalendar/daygrid',
-  '@fullcalendar/list',
-  '@fullcalendar/timegrid'
-])
+// const withTM = require('next-transpile-modules')([
+//   '@fullcalendar/common',
+//   '@fullcalendar/react',
+//   '@fullcalendar/daygrid',
+//   '@fullcalendar/list',
+//   '@fullcalendar/timegrid'
+// ])
 
-module.exports = withTM({
+module.exports = {
   trailingSlash: true,
   reactStrictMode: false,
   experimental: {
     esmExternals: false,
     jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
   },
-  webpack: config => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
-    }
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: {
+        loader: '@svgr/webpack',
+        options: {
+          titleProp: true,
+          titleId: 'filePath'
+        }
+      },
+    })
+  // webpack: config => {
+  //   config.resolve.alias = {
+  //     ...config.resolve.alias,
+  //     apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
+  //   }
 
     return config
   }
-})
+}
--- src/pages/api/recharts-timechart.ts
+++ src/pages/api/recharts-timechart.ts
@@ -8,8 +8,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   const cardId = req.query.cardId
   const code = req.query.code
 
+
+  
   // ** Jsonからデータ取得
-  const response = await fetch(`https://storage.googleapis.com/statistics-hyogo/recharts/${cardId}_${code}.json`)
+  // const response = await fetch(`https://storage.googleapis.com/statistics-hyogo/recharts/${cardId}_${code}.json`)
+  
+  const response = await fetch(`https://storage.googleapis.com/statistics-hyogo/recharts/population-age-prefecture%20_28000.json`)
   const data = await response.json()
 
   res.status(200).json(data)
